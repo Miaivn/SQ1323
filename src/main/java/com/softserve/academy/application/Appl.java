d46ceb96a2f33ee0eeb3d26326887cbc32917716
Solomiia Ivaniuk <150377742+Miaivn@users.noreply.github.com>
2025-03-07 18:51:50 +0200

    Update Appl.java

--- src/main/java/com/softserve/academy/application/Appl.java
+++ src/main/java/com/softserve/academy/application/Appl.java
@@ -1,8 +1,11 @@
 package com.softserve.academy.application;
 
 import com.softserve.academy.service.Calc;
+import java.util.logging.Logger;
 
 public class Appl {
+    // Create a logger instance
+    private static final Logger logger = Logger.getLogger(Appl.class.getName());
 
     public int add(int arg0, int arg1) {
         return arg0 + arg1;
@@ -10,9 +13,12 @@ public class Appl {
 
     public static void main(String[] args) {
         Calc calc = new Calc();
-        System.out.println("1 + 2 = " + calc.add(1, 2));
-        System.out.println("1.5 + 2.5 = " + calc.add(1.5, 2.5));
-        System.out.println("4 - 2 = " + calc.sub(4, 2));
-        System.out.println("2.5 - 1.5 = " + calc.sub(2.5, 1.5));
+
+        // Replace System.out.println with logger
+        logger.info("1 + 2 = " + calc.add(1, 2));
+
+        logger.info("1.5 + 2.5 = " + calc.add(1.5, 2.5));
+        logger.info("4 - 2 = " + calc.sub(4, 2));
+        logger.info("2.5 - 1.5 = " + calc.sub(2.5, 1.5));
     }
 }
